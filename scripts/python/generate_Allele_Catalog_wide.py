4bf58b217bb6e283d21689e81176066f2a50c534
Yen On Chan <yenon118@gmail.com>
2021-10-06 20:51:12 -0500

    Edited generate_Allele_Catalog_wide

--- scripts/python/generate_Allele_Catalog_wide.py
+++ scripts/python/generate_Allele_Catalog_wide.py
@@ -3,10 +3,56 @@
 import sys
 import os
 import re
+import math
 import argparse
 import pathlib
+import threading
+
+from joblib import Parallel, delayed
 
 import pandas as pd
+
+
+# Generate gene array
+# This function is for joblib parallel
+def generate_gene_array(line, gene_array):
+    values = line.strip("\n").split("\t")
+    gene = values[7]
+    if gene not in gene_array:
+        gene_array.append(gene)
+
+
+# Generate wide allele catalog
+# This function is for joblib parallel
+def generate_allele_catalog_wide(input_file_path, genes, output_file_path, lock):
+    df = pd.DataFrame(columns = ['Classification', 'Improvement_Status', 'Maturity_Group', 'Country', 'State', 'Accession', 'Chromosome', 'Gene', 'Position', 'Genotype', 'Genotype_with_Description'])
+    chunksize = 100000
+    for dat in pd.read_table(filepath_or_buffer=input_file_path, chunksize=chunksize):
+        dat = dat[dat['Gene'].isin(genes)]
+        if dat.shape[0] > 0:
+            df = pd.concat([df, dat])
+    lock.acquire()
+    if df.shape[0] > 0:
+        df = df.drop_duplicates(subset=['Accession', 'Chromosome', 'Gene', 'Position'])
+        df = df.sort_values(by=['Accession', 'Chromosome', 'Gene', 'Position'])
+        column_names = df.columns.tolist()
+        subset_of_column_names = df.columns.tolist()
+        subset_of_column_names.remove('Position')
+        subset_of_column_names.remove('Genotype')
+        subset_of_column_names.remove('Genotype_with_Description')
+        df = df.groupby(subset_of_column_names, dropna=False).aggregate({
+            'Position': lambda x: ' '.join(map(str, x)),
+            'Genotype': lambda x: ' '.join(map(str, x)),
+            'Genotype_with_Description': lambda x: ' '.join(map(str, x))
+        }).reset_index()
+        df = df.drop_duplicates(subset=['Accession', 'Chromosome', 'Gene', 'Position'])
+        df = df.loc[:, column_names]
+        df = df.sort_values(by=['Accession', 'Chromosome', 'Gene'])
+        if (df is not None) and (df.shape[0] > 0) and (not output_file_path.exists()):
+            df.to_csv(path_or_buf=output_file_path, sep='\t', index=False, header=True, doublequote=False, mode='w')
+        elif (df is not None) and (df.shape[0] > 0) and (output_file_path.exists()) and (output_file_path.is_file()):
+            df.to_csv(path_or_buf=output_file_path, sep='\t', index=False, header=False, doublequote=False, mode='a')
+    lock.release()
 
 
 def main(args):
@@ -15,6 +61,13 @@ def main(args):
     #######################################################################
     input_file_path = args.input_file
     output_file_path = args.output_file
+
+    n_jobs = args.threads
+
+    #######################################################################
+    # Create a threading lock
+    #######################################################################
+    lock = threading.Lock()
 
     #######################################################################
     # Check if output parent folder exists
@@ -37,60 +90,34 @@ def main(args):
     #######################################################################
     # Collect genes in the input file
     #######################################################################
-    accessions = []
-    genes = []
-    chunksize = 100000
-    for dat in pd.read_table(filepath_or_buffer=input_file_path, usecols=['Accession', 'Gene'], chunksize=chunksize):
-        for accession in dat['Accession']:
-            if accession not in accessions:
-                accessions.append(accession)
-        for gene in dat['Gene']:
-            if gene not in genes:
-                genes.append(gene)
-    accessions.sort()
-    genes.sort()
+    grouped_gene_array = []
+    gene_array = []
+    n = 10
+    with open(input_file_path, 'r') as reader:
+        header = reader.readline()
+        Parallel(n_jobs=n_jobs, backend="threading")(
+            delayed(generate_gene_array)(line, gene_array)
+            for line in reader
+        )
+    if len(gene_array) > 0:
+        for i in range(math.ceil(len(gene_array)/n)):
+            i_start = i*n
+            i_stop = i*n+n if i*n+n < len(gene_array) else len(gene_array)
+            grouped_gene_array.append(gene_array[i_start:i_stop])
+    else:
+        df = pd.DataFrame(
+            columns=['Classification', 'Improvement_Status', 'Maturity_Group', 'Country', 'State', 'Accession',
+                     'Chromosome', 'Gene', 'Position', 'Genotype', 'Genotype_with_Description'])
+        df.to_csv(path_or_buf=output_file_path, sep='\t', index=False, header=True, doublequote=False, mode='w')
+        sys.exit(0)
 
     #######################################################################
     # Generate allele catalog wide
     #######################################################################
-    for gene in genes:
-        # Create a data frame that has header only
-        df = pd.DataFrame(
-            columns=[
-                'Classification', 'Improvement_Status', 'Maturity_Group',
-                'Country', 'State', 'Accession', 'Chromosome', 'Gene',
-                'Position', 'Genotype', 'Genotype_with_Description'
-            ]
-        )
-        # Read data that has that specific gene
-        chunksize = 100000
-        for dat in pd.read_table(filepath_or_buffer=input_file_path, chunksize=chunksize):
-            dat = dat[dat['Gene'] == gene]
-            if dat.shape[0] > 0:
-                df = pd.concat([df, dat], ignore_index=True)
-        # Restructure the data frame
-        if df.shape[0] > 0:
-            df = df.drop_duplicates(subset=['Accession', 'Chromosome', 'Gene', 'Position'])
-            df = df.sort_values(by=['Accession', 'Chromosome', 'Gene', 'Position'])
-            column_names = df.columns.tolist()
-            subset_of_column_names = df.columns.tolist()
-            subset_of_column_names.remove('Position')
-            subset_of_column_names.remove('Genotype')
-            subset_of_column_names.remove('Genotype_with_Description')
-            df = df.groupby(subset_of_column_names, dropna=False).aggregate({
-                'Position': lambda x: ' '.join(map(str, x)),
-                'Genotype': lambda x: ' '.join(map(str, x)),
-                'Genotype_with_Description': lambda x: ' '.join(map(str, x))
-            }).reset_index()
-            df = df.drop_duplicates(subset=['Accession', 'Chromosome', 'Gene', 'Position'])
-            df = df.loc[:, column_names]
-            df = df.sort_values(by=['Accession', 'Chromosome', 'Gene'])
-            if (df is not None) and (df.shape[0] > 0) and (not output_file_path.exists()):
-                df.to_csv(path_or_buf=output_file_path, sep='\t', index=False, header=True, doublequote=False, mode='w')
-            elif (df is not None) and (df.shape[0] > 0) and (output_file_path.exists()) and (
-            output_file_path.is_file()):
-                df.to_csv(path_or_buf=output_file_path, sep='\t', index=False, header=False, doublequote=False,
-                          mode='a')
+    Parallel(n_jobs=n_jobs, backend="threading")(
+        delayed(generate_allele_catalog_wide)(input_file_path, genes, output_file_path, lock)
+        for genes in grouped_gene_array 
+    )
     # If the output file still does not exists, write an empty table with header only
     if not output_file_path.exists():
         df = pd.DataFrame(
@@ -107,6 +134,8 @@ if __name__ == "__main__":
 
     parser.add_argument('-i', '--input_file', help='Input file', type=pathlib.Path, required=True)
     parser.add_argument('-o', '--output_file', help='Output file', type=pathlib.Path, required=True)
+
+    parser.add_argument('-t', '--threads', help='Number of threads', type=int, default=10)
 
     args = parser.parse_args()
 
